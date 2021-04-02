30b1f93d9f32c9e17f869c7581b82a5aab397f09
DavidRMorphew <morphewd@umich.edu>
2021-04-02 17:07:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(arrayOfYearAndResult){
+    const firstWinningYear = arrayOfYearAndResult.find((yearResultObj) => {
+        return yearResultObj.result === "W"
+    })
+    console.log(firstWinningYear)
+    if (firstWinningYear) {
+        return firstWinningYear.year
+    } else {
+        return undefined
+    }
+}
